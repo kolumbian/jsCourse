189aded7e2eb95db1ee915b33b8d57f995854c3b
Serhii <ikokulov@gmail.com>
2021-04-14 13:23:55 +0300

    complete lesson

--- 2nd part/script.js
+++ 2nd part/script.js
@@ -1,18 +1,18 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "Фильмов во мне дохуя");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "Фильмов во мне дохуя");
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat = false
+    private: false
 };
 
 const firstQuestionFilm = prompt("Один из последних просмотренных фильмов?","");
-const firstQuestionRate = prompt("На сколько оцените его?","");
+const firstQuestionRate = +prompt("На сколько оцените его?","");
 
 const secondQuestionFilm = prompt("Ещё один из последних просмотренных фильмов?","");
-const secondQuestionRate = prompt("Один из последних просмотренных фильмов?","");
+const secondQuestionRate = +prompt("На сколько оцените его?","");
 
 personalMovieDB.movies[firstQuestionFilm] = firstQuestionRate;
 personalMovieDB.movies[secondQuestionFilm] = secondQuestionRate;
