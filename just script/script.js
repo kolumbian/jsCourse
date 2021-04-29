776f05e6b5477fbce2c96369ce818d8fe9a9d68b
Serhii <ikokulov@gmail.com>
2021-04-29 17:53:36 +0300

    almost 4 practice

--- just script/script.js
+++ just script/script.js
@@ -70,29 +70,165 @@
 
 // METHODS AND CONFIGURATIONS
 
-const str = "teSt";
-const arr = [1, 2, 4];
+// const str = "teSt";
+// const arr = [1, 2, 4];
 
 // console.log(str[3]);
 // console.log(str.toUpperCase());
 // console.log(str.toLowerCase());
 
-console.log(str);
+// console.log(str);
 
-const fruit = "Some fruit";
+// const fruit = "Some fruit";
 
-console.log(fruit.indexOf("x"));
+// console.log(fruit.indexOf("x"));
 
-const logg = "Hello plplplp";
+// const logg = "Hello plplplp";
 
 // console.log(logg.slice(5));
-console.log(logg.substring(5));
-console.log(logg.substr(6, 7));
+// console.log(logg.substring(5));
+// console.log(logg.substr(6, 7));
 
-const num = 12.7;
-console.log(Math.round(num));
+// const num = 12.7;
+// console.log(Math.round(num));
 
 
-const test = "12.745px";
-console.log(parseInt(test));
-console.log(parseFloat(test));
+// const test = "12.745px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+// ARRAYS AND PSEUDOARRAYS
+
+// const arr = [2, 3, 6, 8, 10];
+// arr[99] = 0;
+// console.log(arr.length);
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
+
+
+// Передача данных по ссылке и по значению
+
+// let a = 5;
+//     b = a;
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'd';
+
+// console.log(newArray);
+// console.log(oldArray);
+
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+const array = ["a", "b"];
+
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+
+console.log(q);
